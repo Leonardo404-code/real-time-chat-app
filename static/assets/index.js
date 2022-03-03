005c00d179aaf651e24387b21d7e293b48508837
Leonardo Bispo <leonardobispo1000@gmail.com>
2022-03-03 14:27:48 -0300

    Add user feature

--- static/assets/index.js
+++ static/assets/index.js
@@ -1,9 +1,11 @@
-let socket = new WebSocket("ws://127.0.0.1:8080/ws");
 const chat = document.querySelector("#chat");
 const input = document.querySelector("#messages");
 const send = document.querySelector("#send");
-
-console.log("Attempting Connection...");
+const userModal = document.querySelector("#user");
+const usernameInput = document.querySelector("#username");
+const addUserButton = document.querySelector("#addUser");
+const addUserClient = document.querySelector("#newClient");
+const username = localStorage.getItem("user");
 
 const insertMessage = (msg, chat) => {
   const message = document.createElement("p");
@@ -15,34 +17,70 @@ const insertMessage = (msg, chat) => {
   chat.appendChild(message);
 };
 
-socket.onopen = () => {
-  socket.send("Novo usuário conectado");
+const Connect = (username) => {
+  console.log("Attempting Connection...");
+
+  let socket = new WebSocket("ws://127.0.0.1:8080/ws");
+
+  socket.onopen = () => {
+    socket.send(`${username} Conectado`);
+  };
+
+  socket.onclose = () => {
+    socket.send("Client Closed!");
+  };
+
+  socket.onerror = (error) => {
+    console.log("Socket Error: ", error);
+  };
+
+  socket.onmessage = (msg) => {
+    const { data } = msg;
+
+    insertMessage(data, chat);
+  };
+
+  send.onclick = () => {
+    const message = input.value;
+
+    if (message === "") {
+      return;
+    }
+
+    socket.send(`${username}: ${message}`);
+    input.value = "";
+  };
+
+  input.addEventListener("keyup", (e) => {
+    if (e.keyCode === 13) {
+      send.click();
+    }
+  });
 };
 
-socket.onclose = (event) => {
-  console.log("Socket Closed Connection: ", event);
-  socket.send("Client Closed!");
-};
+if (username === null) {
+  addUserButton.addEventListener("click", (e) => {
+    e.preventDefault();
 
-socket.onerror = (error) => {
-  console.log("Socket Error: ", error);
-};
+    const value = usernameInput.value;
 
-socket.onmessage = (msg) => {
-  const { data } = msg;
+    if (e.keyCode === 13) {
+      localStorage.setItem("user", value);
+      userModal.style.display = "none";
+      Connect(value);
+    }
 
-  insertMessage(data, chat);
-};
+    localStorage.setItem("user", value);
+    userModal.style.display = "none";
+    Connect(value);
+  });
 
-send.onclick = () => {
-  const message = input.value;
-
-  socket.send(message);
-  input.value = "";
-};
-
-input.addEventListener("keyup", (e) => {
-  if (e.keyCode === 13) {
-    send.click();
-  }
-});
+  addUserClient.addEventListener("click", () => {
+    localStorage.setItem("user", "Usuário");
+    userModal.style.display = "none";
+    Connect("Usuário");
+  });
+} else {
+  userModal.style.display = "none";
+  Connect(username);
+}
